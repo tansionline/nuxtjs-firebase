af36dcc7ccb42087f720e18259b849f4a29332c7
tansionline <tatarselcuk27@gmail.com>
2021-04-17 19:02:19 +0300

    firestore page updated

--- pages/firestore-sayfasi.vue
+++ pages/firestore-sayfasi.vue
@@ -1,5 +1,10 @@
 <script>
 export default {
+  data() {
+    return {
+      mesaj: "",
+    };
+  },
   methods: {
     async firestoreYaz() {
       const messageRef = this.$fire.firestore
@@ -7,7 +12,7 @@ export default {
         .doc("dokuman-adi");
       try {
         await messageRef.set({
-          mesaj: "Selam Dunya",
+          mesaj: this.mesaj,
         });
       } catch (e) {
         alert(e);
@@ -21,6 +26,9 @@ export default {
 
 <template>
   <div class="container">
+    <form>
+      <textarea v-model="mesaj"></textarea>
+    </form>
     <button v-on:click="firestoreYaz">Gonder</button>
   </div>
 </template>
@@ -29,6 +37,18 @@ export default {
 .container {
   padding: 2rem;
   text-align: center;
+}
+
+textarea {
+  width: 100%;
+  height: 150px;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  font-size: 16px;
+  resize: none;
 }
 
 button {

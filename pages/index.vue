9bcee6a71c4dd549d05d710401b8f5f916ecf4ea
tansionline <tatarselcuk27@gmail.com>
2021-04-17 18:39:47 +0300

    index updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
+<script>
+export default {
+  methods: {
+    async writeToFirestore() {
+      const messageRef = this.$fire.firestore
+        .collection("gelen-mesajlar")
+        .doc("dokuman-adi");
+      try {
+        await messageRef.set({
+          mesaj: "Selam Dunya",
+        });
+      } catch (e) {
+        alert(e);
+        return;
+      }
+      alert("Success.");
+    },
+  },
+};
+</script>
+
 <template>
   <div class="container">
     <a href="/kayit-ol">Kayit Ol</a>
@@ -5,6 +26,22 @@
   </div>
 </template>
 
-<script>
-export default {};
-</script>
+<style scoped>
+.container {
+  padding: 2rem;
+  text-align: center;
+}
+
+a {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  background-color: #008cba;
+}
+</style>
